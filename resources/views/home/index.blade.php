0bc2271677c1d29f5c7c620595c74bad6192e31a
MudassirAdmani <mudassiradmani466@gmail.com>
2024-01-25 21:57:56 +0500

    added

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -165,21 +165,21 @@
             <div class="col-lg-12 bg-white shadow p-4 rounded">
                 <form>
                     <div class="d-flex justify-content-between align-items-center">
-                        <h5>Check Booking Availability</h5>
+                        <h5>Check Doctor Availability</h5>
                         <button type="submit" class="btn btn-danger">Check</button>
                     </div>
                     <hr>
                     <div class="row">
                         <div class="col-lg-3 mb-3">
-                            <label class="form-label" style="font-weight: 500;">Check In</label>
+                            <label class="form-label" style="font-weight: 500;">Date</label>
                             <input type="date" class="form-control shadow-none">
                         </div>
                         <div class="col-lg-3 mb-3">
-                            <label class="form-label" style="font-weight: 500;">Check Out</label>
+                            <label class="form-label" style="font-weight: 500;">Doctor</label>
                             <input type="date" class="form-control shadow-none">
                         </div>
                         <div class="col-lg-3 mb-3">
-                            <label class="form-label" style="font-weight: 500;">Adults</label>
+                            <label class="form-label" style="font-weight: 500;">Time</label>
                             <select class="form-select shadow-none">
                                 <option value="1">One</option>
                                 <option value="2">Two</option>
@@ -187,7 +187,7 @@
                             </select>
                         </div>
                         <div class="col-lg-3 mb-3">
-                            <label class="form-label" style="font-weight: 500;">Childrens</label>
+                            <label class="form-label" style="font-weight: 500;">Type</label>
                             <select class="form-select shadow-none">
                                 <option value="1">One</option>
                                 <option value="2">Two</option>
