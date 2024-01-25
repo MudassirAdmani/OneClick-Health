b8c555567952b162f7d8142e7d25ac7b16c1c49e
MudassirAdmani <mudassiradmani466@gmail.com>
2024-01-25 21:55:40 +0500

    Availability Form

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -160,6 +160,45 @@
             </div>
         </div>
     </div>
+    <div class="container AvailabilityForm">
+        <div class="row">
+            <div class="col-lg-12 bg-white shadow p-4 rounded">
+                <form>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <h5>Check Booking Availability</h5>
+                        <button type="submit" class="btn btn-danger">Check</button>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-lg-3 mb-3">
+                            <label class="form-label" style="font-weight: 500;">Check In</label>
+                            <input type="date" class="form-control shadow-none">
+                        </div>
+                        <div class="col-lg-3 mb-3">
+                            <label class="form-label" style="font-weight: 500;">Check Out</label>
+                            <input type="date" class="form-control shadow-none">
+                        </div>
+                        <div class="col-lg-3 mb-3">
+                            <label class="form-label" style="font-weight: 500;">Adults</label>
+                            <select class="form-select shadow-none">
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                        <div class="col-lg-3 mb-3">
+                            <label class="form-label" style="font-weight: 500;">Childrens</label>
+                            <select class="form-select shadow-none">
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
     <!-- Bootstrap -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js" integrity="sha512-X/YkDZyjTf4wyc2Vy16YGCPHwAY8rZJY+POgokZjQB2mhIRFJCckEGc6YyX9eNsPfn0PzThEuNs+uaomE5CO6A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- Swiper -->
