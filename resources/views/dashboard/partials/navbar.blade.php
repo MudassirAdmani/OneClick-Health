c9829c3f03e2aaadc27d7346593971f43a029eb2
MudassirAdmani <mudassiradmani466@gmail.com>
2024-01-27 20:44:37 +0500

    Laravel

--- resources/views/dashboard/partials/navbar.blade.php
+++ resources/views/dashboard/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar p-0 fixed-top d-flex flex-row">
     <div class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
-        <a class="navbar-brand brand-logo-mini" href="index.html"><img src="dashboard/assets/images/logo-mini.svg" alt="logo" /></a>
+        <a class="navbar-brand brand-logo-mini" href="index.html"><img src="assets/images/logo-mini.svg" alt="logo" /></a>
     </div>
     <div class="navbar-menu-wrapper flex-grow d-flex align-items-stretch">
         <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
@@ -15,7 +15,8 @@
         </ul>
         <ul class="navbar-nav navbar-nav-right">
             <li class="nav-item dropdown d-none d-lg-block">
-                <a class="nav-link btn btn-success create-new-button" id="createbuttonDropdown" data-bs-toggle="dropdown" aria-expanded="false" href="#">+ Create New Project</a>
+                <a class="nav-link btn btn-success create-new-button" id="createbuttonDropdown" data-bs-toggle="dropdown" aria-expanded="false" href="#">+ Create New
+                    Project</a>
                 <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="createbuttonDropdown">
                     <h6 class="p-3 mb-0">Projects</h6>
                     <div class="dropdown-divider"></div>
@@ -70,7 +71,7 @@
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item preview-item">
                         <div class="preview-thumbnail">
-                            <img src="dashboard/assets/images/faces/face4.jpg" alt="image" class="rounded-circle profile-pic">
+                            <img src="assets/images/faces/face4.jpg" alt="image" class="rounded-circle profile-pic">
                         </div>
                         <div class="preview-item-content">
                             <p class="preview-subject ellipsis mb-1">Mark send you a message</p>
@@ -80,7 +81,7 @@
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item preview-item">
                         <div class="preview-thumbnail">
-                            <img src="dashboard/assets/images/faces/face2.jpg" alt="image" class="rounded-circle profile-pic">
+                            <img src="assets/images/faces/face2.jpg" alt="image" class="rounded-circle profile-pic">
                         </div>
                         <div class="preview-item-content">
                             <p class="preview-subject ellipsis mb-1">Cregh send you a message</p>
@@ -90,7 +91,7 @@
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item preview-item">
                         <div class="preview-thumbnail">
-                            <img src="dashboard/assets/images/faces/face3.jpg" alt="image" class="rounded-circle profile-pic">
+                            <img src="assets/images/faces/face3.jpg" alt="image" class="rounded-circle profile-pic">
                         </div>
                         <div class="preview-item-content">
                             <p class="preview-subject ellipsis mb-1">Profile picture updated</p>
@@ -117,7 +118,8 @@
                         </div>
                         <div class="preview-item-content">
                             <p class="preview-subject mb-1">Event today</p>
-                            <p class="text-muted ellipsis mb-0"> Just a reminder that you have an event today </p>
+                            <p class="text-muted ellipsis mb-0"> Just a reminder that you have an event
+                                today </p>
                         </div>
                     </a>
                     <div class="dropdown-divider"></div>
@@ -148,41 +150,14 @@
                     <p class="p-3 mb-0 text-center">See all notifications</p>
                 </div>
             </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link" id="profileDropdown" href="#" data-bs-toggle="dropdown">
-                    <div class="navbar-profile">
-                        <img class="img-xs rounded-circle" src="dashboard/assets/images/faces/face15.jpg" alt="">
-                        <p class="mb-0 d-none d-sm-block navbar-profile-name">Henry Klein</p>
-                        <i class="mdi mdi-menu-down d-none d-sm-block"></i>
-                    </div>
-                </a>
-                <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="profileDropdown">
-                    <h6 class="p-3 mb-0">Profile</h6>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item preview-item">
-                        <div class="preview-thumbnail">
-                            <div class="preview-icon bg-dark rounded-circle">
-                                <i class="mdi mdi-settings text-success"></i>
-                            </div>
-                        </div>
-                        <div class="preview-item-content">
-                            <p class="preview-subject mb-1">Settings</p>
-                        </div>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item preview-item">
-                        <div class="preview-thumbnail">
-                            <div class="preview-icon bg-dark rounded-circle">
-                                <i class="mdi mdi-logout text-danger"></i>
-                            </div>
-                        </div>
-                        <div class="preview-item-content">
-                            <p class="preview-subject mb-1">Log out</p>
-                        </div>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <p class="p-3 mb-0 text-center">Advanced settings</p>
-                </div>
+            <li class="nav-item me-1">
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <input type="submit" class="btn btn-danger" value="Logout">
+                </form>
+            </li>
+            <li class="nav-item ms-1">
+                <a class="btn btn-info" href="{{ route('profile.show') }}">Profile</a>
             </li>
         </ul>
         <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
