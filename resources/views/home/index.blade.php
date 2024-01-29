701232881625100c255f8ccb1a783ac04d30fd9c
MudassirAdmani <mudassiradmani466@gmail.com>
2024-01-30 04:45:25 +0500

    yes late night work baby

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -10,7 +10,7 @@
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" integrity="sha512-b2QcS5SsA8tZodcDtGRELiGv5SaKSk1vDHDaQRda0htPYWZ6046lr3kJ5bAAQdpV2mmA/4v0wQF9MyU6/pDIAg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <!-- CSS -->
     <!-- Master -->
     <link rel="stylesheet" href="css/style.css">
@@ -31,6 +31,8 @@
     @include('home.partials.availability')
     <!-- Insurance -->
     @include('home.partials.insurance')
+    <!-- Contact -->
+    @include('home.partials.contact')
     <!-- Bootstrap -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js" integrity="sha512-X/YkDZyjTf4wyc2Vy16YGCPHwAY8rZJY+POgokZjQB2mhIRFJCckEGc6YyX9eNsPfn0PzThEuNs+uaomE5CO6A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- Swiper -->
