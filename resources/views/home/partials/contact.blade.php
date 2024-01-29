ac36c719d654c78e95a209351c8759c71cbdfe20
MudassirAdmani <mudassiradmani466@gmail.com>
2024-01-30 04:45:38 +0500

    coding is love yeahhhh

---
+++ resources/views/home/partials/contact.blade.php
@@ -0,0 +1,43 @@
+<div class="container">
+    <h2 class="text-center my-4">Contact Us</h2>
+    <div class="row">
+        <div class="col-lg-4 mt-3 mt-lg-0">
+            <div class="border border-1 border-secondary p-3">
+                <h3 class="text-secondary">Address</h3>
+                <p class="text-secondary mt-4">Company Name INC.</p>
+                <p class="text-secondary">423 Avenue Street, Chicago, IL 60610</p>
+            </div>
+        </div>
+        <div class="col-lg-4 mt-3 mt-lg-0">
+            <div class="border border-1 border-secondary p-3">
+                <h3 class="text-secondary">Phone</h3>
+                <p class="text-secondary mt-4"><span class="fw-bold">Office:</span> +1 800 321 431</p>
+                <p class="text-secondary"><span class="fw-bold">Support:</span> +1 800 903 412</p>
+            </div>
+        </div>
+        <div class="col-lg-4 mt-3 mt-lg-0">
+            <div class="border border-1 border-secondary p-3">
+                <h3 class="text-secondary">Email</h3>
+                <p class="text-secondary mt-4"><span class="fw-bold">Office:</span> office@companyname.com</p>
+                <p class="text-secondary"><span class="fw-bold">Support:</span> support@companyname.com</p>
+            </div>
+        </div>
+    </div>
+    <form>
+        <div class="row mt-lg-5 mt-3">
+            <div class="col-lg-4">
+                <input class="form-control py-2 border-secondary" type="text" placeholder="Your Name">
+                <input class="form-control my-3 py-2 border-secondary" type="text" placeholder="Your Name">
+                <input class="form-control my-3 py-2 border-secondary" type="text" placeholder="Your Name">
+            </div>
+            <div class="col-lg-8">
+                <textarea class="w-100" cols="90" rows="7"></textarea>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <button class="float-end btn btn-danger">Send Message</button>
+            </div>
+        </div>
+    </form>
+</div>
---
+++ resources/views/home/partials/insurance.blade.php
@@ -0,0 +1,59 @@
+<div class="container">
+    <h2 class="text-center my-4">Insurance Plans</h2>
+    <div class="row">
+        <div class="col-4">
+            <div class="bg-danger d-flex justify-content-center align-items-center py-3">
+                <h4 class="text-white">Pro Plan</h4>
+            </div>
+            <div class="border border-1 border-danger">
+                <div class="bg-gray py-4">
+                    <h2 class="text-center">10$ / Month</h2>
+                </div>
+                <div class="text-center">
+                    <p class="border-bottom pt-4 border-1">Unlimited Use</p>
+                    <p class="border-bottom pt-2 border-1">Unlimited Use</p>
+                    <p class="border-bottom pt-2 border-1">Unlimited Use</p>
+                </div>
+            </div>
+            <div class="bg-danger d-flex justify-content-center align-items-center py-3">
+                <h4 class="text-white">Buy Now</h4>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="bg-primary d-flex justify-content-center align-items-center py-3">
+                <h4 class="text-white">Pro Plan</h4>
+            </div>
+            <div class="border border-1 border-primary">
+                <div class="bg-gray py-4">
+                    <h2 class="text-center">10$ / Month</h2>
+                </div>
+                <div class="text-center">
+                    <p class="border-bottom pt-4 border-1">Unlimited Use</p>
+                    <p class="border-bottom pt-2 border-1">Unlimited Use</p>
+                    <p class="border-bottom pt-2 border-1">Unlimited Use</p>
+                </div>
+            </div>
+            <div class="bg-primary d-flex justify-content-center align-items-center py-3">
+                <h4 class="text-white">Buy Now</h4>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="bg-success d-flex justify-content-center align-items-center py-3">
+                <h4 class="text-white">Pro Plan</h4>
+            </div>
+            <div class="border border-1 border-success">
+                <div class="bg-gray py-4">
+                    <h2 class="text-center">10$ / Month</h2>
+                </div>
+                <div class="text-center">
+                    <p class="border-bottom pt-4 border-1">Unlimited Use</p>
+                    <p class="border-bottom pt-2 border-1">Unlimited Use</p>
+                    <p class="border-bottom pt-2 border-1">Unlimited Use</p>
+                </div>
+            </div>
+            <div class="bg-success d-flex justify-content-center align-items-center py-3">
+                <h4 class="text-white">Buy Now</h4>
+            </div>
+        </div>
+    </div>
+</div>
