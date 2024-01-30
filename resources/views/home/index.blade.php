4b13b362b712ac2b8df6457f8571a8fb55ea6d4e
MudassirAdmani <mudassiradmani466@gmail.com>
2024-01-31 02:09:40 +0500

    love my work

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -31,6 +31,8 @@
     @include('home.partials.availability')
     <!-- Insurance -->
     @include('home.partials.insurance')
+    <!-- Footer -->
+    @include('home.partials.footer')
     <!-- Bootstrap -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js" integrity="sha512-X/YkDZyjTf4wyc2Vy16YGCPHwAY8rZJY+POgokZjQB2mhIRFJCckEGc6YyX9eNsPfn0PzThEuNs+uaomE5CO6A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- Swiper -->
