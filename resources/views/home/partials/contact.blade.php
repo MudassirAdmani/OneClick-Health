0cf4ee3dac216286a6f40f4b4f7bea412cc27788
MudassirAdmani <mudassiradmani466@gmail.com>
2024-01-31 02:00:28 +0500

    contact form completed

--- resources/views/home/partials/contact.blade.php
+++ resources/views/home/partials/contact.blade.php
@@ -1,4 +1,8 @@
 <div class="container">
+    <h2 class="text-center my-4">Our Location</h2>
+    <div class="row">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3618.1187375112536!2d67.074406974432!3d24.92802464255253!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33f41305d7149%3A0xedf1eef2371cbf4c!2sOneClick%20Digital%20Insurance%20Aggregator!5e0!3m2!1sen!2s!4v1706647566156!5m2!1sen!2s" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
     <h2 class="text-center my-4">Contact Us</h2>
     <div class="row">
         <div class="col-lg-4 mt-3 mt-lg-0">
