b46d4cbc3c54ee389659ab41be1ac572d5219c87
MudassirAdmani <mudassiradmani466@gmail.com>
2024-01-31 02:09:53 +0500

    Yey i am a best

---
+++ resources/views/home/partials/footer.blade.php
@@ -0,0 +1,66 @@
+<div class="container-fluid bg-gray mt-3">
+    <div class="row justify-content-center">
+        <div class="col-lg-4 p-4 text-white">
+            <h3 class="mb-3 h-font fs-3">OneClick Health</h3>
+            <p>
+                Oneclick Health will operate as a comprehensive
+                healthcare platform, facilitating convenient
+                access to medical services. The business will
+                focus on delivering quality pharmaceuticals
+                through both online orders and a network of
+                nationwide offline pharmacy stores.
+            </p>
+        </div>
+        <div class="col-lg-2 p-4 text-white">
+            <h5 class="mb-3 h-font fs-3">Links</h5>
+            <a href="/home" class="text-white d-inline-block mb-2 text-dark text-decoration-none">Home</a>
+            <br>
+            <a href="/service" class="text-white d-inline-block mb-2 text-dark text-decoration-none">Service</a>
+            <br>
+            <a href="/contact" class="text-white d-inline-block mb-2 text-dark text-decoration-none">Contact</a>
+        </div>
+        <div class="col-lg-2 p-4 text-white">
+            <h5 class="mb-3 fs-3 h-font">Follow Us</h5>
+            <a href="#" class="text-white d-inline-block text-dark text-decoration-none mb-2">
+                <i class="bi bi-instagram me-1"></i>
+                Instagram
+            </a>
+            <br>
+            <a href="#" class="text-white d-inline-block text-dark text-decoration-none mb-2">
+                <i class="bi bi-facebook me-1"></i>
+                Facebook
+            </a>
+            <br>
+            <a href="#" class="text-white d-inline-block text-dark text-decoration-none mb-2">
+                <i class="bi bi-twitter me-1"></i>
+                Twitter
+            </a>
+            <br>
+        </div>
+        <div class="col-lg-2 p-4 text-white">
+            <h5 class="mb-3 fs-3 h-font">Contact Us</h5>
+            <dl>
+                <dt>
+                    Phone:
+                </dt>
+                <dd>
+                    03120293322
+                </dd>
+                <dt>
+                    Email:
+                </dt>
+                <dd>
+                    mudassiradmani466@gmail.com
+                </dd>
+                <dt>
+                    Address:
+                </dt>
+                <dd>
+                    Garden East, Karachi Pakistan
+                </dd>
+            </dl>
+        </div>
+        <div class="col-lg-1"></div>
+    </div>
+</div>
+<h6 class="text-center bg-gray text-white p-3 m-0">Copyrights @ Mudassir Admani</h6>
