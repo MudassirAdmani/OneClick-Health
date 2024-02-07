41ef8b98596a7b2483dbc1365ec294b98056f85a
MudassirAdmani <mudassiradmani466@gmail.com>
2024-02-08 02:19:08 +0500

    so we out living life in the night

--- resources/views/home/partials/insurance.blade.php
+++ resources/views/home/partials/insurance.blade.php
@@ -1,45 +1,46 @@
 <div class="container">
     <h2 class="text-center my-4">Insurance Plans</h2>
     <div class="row justify-content-center">
-        <div class="col-3">
-            <div class="shadow">
-                <div class="bg-danger rounded-top p-2">
-                    <h3 class="text-center text-white">Basic Plan</h3>
-                </div>
-                <p class="border-bottom text-center fw-bold mt-3">Unlimited Use</p>
-                <p class="border-bottom text-center fw-bold">Unlimited Use</p>
-                <p class="border-bottom text-center fw-bold">Unlimited Use</p>
-                <div class="bg-danger rounded-bottom p-2">
-                    <h3 class="text-center text-white">10$/Month</h3>
-                </div>
+        <div class="col-4">
+            <div class="border shadow-sm rounded p-4">
+                <h5>Basic</h5>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <h3><span class="fw-bold">10$</span><span class="h6 fw-bold">/month</span></h3>
+                <a href="{{ url('/insurance', ['plan' => 'Basic']) }}" class="btn btn-danger form-control">Get Started</a>
+                <hr>
+                <h5>What's Included</h5>
+                <p><i class="bi bi-check-lg text-success"></i>Service 1</p>
+                <p><i class="bi bi-check-lg text-success"></i>Service 2</p>
+                <p><i class="bi bi-x-lg text-danger"></i>Service 3</p>
+                <p><i class="bi bi-x-lg text-danger"></i>Service 4</p>
             </div>
         </div>
-        <div class="col-1"></div>
-        <div class="col-3">
-            <div class="shadow">
-                <div class="bg-danger rounded-top p-2">
-                    <h3 class="text-center text-white">Basic Plan</h3>
-                </div>
-                <p class="border-bottom text-center fw-bold mt-3">Unlimited Use</p>
-                <p class="border-bottom text-center fw-bold">Unlimited Use</p>
-                <p class="border-bottom text-center fw-bold">Unlimited Use</p>
-                <div class="bg-danger rounded-bottom p-2">
-                    <h3 class="text-center text-white">10$/Month</h3>
-                </div>
+        <div class="col-4">
+            <div class="border shadow-sm rounded p-4">
+                <h5>Basic</h5>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <h3><span class="fw-bold">10$</span><span class="h6 fw-bold">/month</span></h3>
+                <a class="btn btn-danger form-control">Get Started</a>
+                <hr>
+                <h5>What's Included</h5>
+                <p><i class="bi bi-check-lg text-success"></i>Service 1</p>
+                <p><i class="bi bi-check-lg text-success"></i>Service 2</p>
+                <p><i class="bi bi-x-lg text-danger"></i>Service 3</p>
+                <p><i class="bi bi-x-lg text-danger"></i>Service 4</p>
             </div>
         </div>
-        <div class="col-1"></div>
-        <div class="col-3">
-            <div class="shadow">
-                <div class="bg-danger rounded-top p-2">
-                    <h3 class="text-center text-white">Basic Plan</h3>
-                </div>
-                <p class="border-bottom text-center fw-bold mt-3">Unlimited Use</p>
-                <p class="border-bottom text-center fw-bold">Unlimited Use</p>
-                <p class="border-bottom text-center fw-bold">Unlimited Use</p>
-                <div class="bg-danger rounded-bottom p-2">
-                    <h3 class="text-center text-white">10$/Month</h3>
-                </div>
+        <div class="col-4">
+            <div class="border shadow-sm rounded p-4">
+                <h5>Basic</h5>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <h3><span class="fw-bold">10$</span><span class="h6 fw-bold">/month</span></h3>
+                <a class="btn btn-danger form-control">Get Started</a>
+                <hr>
+                <h5>What's Included</h5>
+                <p><i class="bi bi-check-lg text-success"></i>Service 1</p>
+                <p><i class="bi bi-check-lg text-success"></i>Service 2</p>
+                <p><i class="bi bi-x-lg text-danger"></i>Service 3</p>
+                <p><i class="bi bi-x-lg text-danger"></i>Service 4</p>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\InsuranceController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
@@ -96,3 +97,8 @@ Route::get('/editproduct/{id}', [ProductController::class, 'editproduct']);
 
 // Update
 Route::post('/updateproduct/{id}', [ProductController::class, 'updateproduct']);
+
+// Insurance Crud
+
+// Add
+Route::get('/addinsurance/{plan}', [InsuranceController::class, 'addinsurance']);
