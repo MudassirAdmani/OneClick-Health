ec3e3fe80d55307b347cf686e9b2b90607fcff88
MudassirAdmani <mudassiradmani466@gmail.com>
2024-02-08 21:39:09 +0500

    we been wake fpr days

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('usertype')->default(0);
+            $table->string('usertype')->default('user');
             $table->string('phone')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamp('email_verified_at')->nullable();
